7f91c127b4a418b5467c23ea90de001610d94d15
Matt R Bonnette <mattrbonnette@pm.me>
2024-12-27 02:04:09 -0500

    Initial commit.

---
+++ build.zig
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright © 2024 Maxine R Bonnette. All rights reserved.
+
+pub const Builddef = @import("src/Builddef.zig");
+
+pub fn build(_: *@import("std").Build) void {}
