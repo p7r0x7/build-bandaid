6e3c52acca7fa7fa6b5432bb20e5c90f5d5b479c
Maxine R Bonnette <mattrbonnette@pm.me>
2025-03-04 14:54:53 -0500

    Add test().

--- src/Builddef.zig
+++ src/Builddef.zig
@@ -13,7 +13,6 @@ build: *Build,
 pub fn init(def: *Build) @This() {
     return .{ .build = def };
 }
-
 pub fn stdOptions(
     def: *@This(),
     target_opts: Build.StandardTargetOptionsArgs,
@@ -24,7 +23,6 @@ pub fn stdOptions(
     def.universalSettings(target);
     return .{ target, optimize };
 }
-
 pub fn universalSettings(def: *@This(), target: Target) void {
     const t = target.result.cpu.arch == .x86_64;
     def.build.enable_wine = t and target.result.os.tag == .windows;
@@ -37,14 +35,40 @@ pub fn runArtifact(def: *@This(), comp: *Compile, args: ?[]const []const u8) *Bu
     if (args) |v| run.addArgs(v);
     return run;
 }
-
-pub fn compile(def: *@This(), step: anytype, name: []const u8, root: ?[]const u8, target: Target, optimize: Mode) *Compile {
-    const comp = step(def.build, .{
+pub fn @"test"(def: *@This(), root: ?[]const u8, target: Target, optimize: Mode) *Compile {
+    const comp = Build.addTest(def.build, .{
+        .unwind_tables = if (optimize == .Debug or optimize == .ReleaseSafe) .sync else .none,
         .omit_frame_pointer = optimize == .Debug or optimize == .ReleaseSafe,
         .error_tracing = optimize == .Debug or optimize == .ReleaseSafe,
         .strip = optimize == .ReleaseFast or optimize == .ReleaseSmall,
         .root_source_file = if (root) |v| def.build.path(v) else null,
-        .unwind_tables = if (optimize == .Debug) .sync else .none,
+        .optimize = optimize,
+        .target = target,
+        .pic = true,
+    });
+    comp.want_lto = !target.result.os.tag.isDarwin(); // https://github.com/ziglang/zig/issues/8680
+    comp.compress_debug_sections = .zstd;
+    comp.link_function_sections = true;
+    comp.link_data_sections = true;
+    comp.link_gc_sections = true;
+    return comp;
+}
+pub fn executable(def: *@This(), name: []const u8, root: ?[]const u8, target: Target, optimize: Mode) *Compile {
+    return def.compile(Build.addExecutable, name, root, target, optimize);
+}
+pub fn staticLib(def: *@This(), name: []const u8, root: ?[]const u8, target: Target, optimize: Mode) *Compile {
+    return def.compile(Build.addStaticLibrary, name, root, target, optimize);
+}
+pub fn sharedLib(def: *@This(), name: []const u8, root: ?[]const u8, target: Target, optimize: Mode) *Compile {
+    return def.compile(Build.addSharedLibrary, name, root, target, optimize);
+}
+pub fn compile(def: *@This(), step: anytype, name: []const u8, root: ?[]const u8, target: Target, optimize: Mode) *Compile {
+    const comp = step(def.build, .{
+        .unwind_tables = if (optimize == .Debug or optimize == .ReleaseSafe) .sync else .none,
+        .omit_frame_pointer = optimize == .Debug or optimize == .ReleaseSafe,
+        .error_tracing = optimize == .Debug or optimize == .ReleaseSafe,
+        .strip = optimize == .ReleaseFast or optimize == .ReleaseSmall,
+        .root_source_file = if (root) |v| def.build.path(v) else null,
         .optimize = optimize,
         .target = target,
         .name = name,
@@ -56,18 +80,6 @@ pub fn compile(def: *@This(), step: anytype, name: []const u8, root: ?[]const u8
     comp.link_data_sections = true;
     comp.link_gc_sections = true;
     return comp;
-}
-
-pub fn executable(def: *@This(), name: []const u8, root: ?[]const u8, target: Target, optimize: Mode) *Compile {
-    return def.compile(Build.addExecutable, name, root, target, optimize);
-}
-
-pub fn staticLib(def: *@This(), name: []const u8, root: ?[]const u8, target: Target, optimize: Mode) *Compile {
-    return def.compile(Build.addStaticLibrary, name, root, target, optimize);
-}
-
-pub fn sharedLib(def: *@This(), name: []const u8, root: ?[]const u8, target: Target, optimize: Mode) *Compile {
-    return def.compile(Build.addSharedLibrary, name, root, target, optimize);
 }
 
 pub fn addCSources(def: *@This(), comp: *Compile, sources: []const CSources) void {
@@ -84,7 +96,6 @@ pub fn addCSources(def: *@This(), comp: *Compile, sources: []const CSources) voi
         });
     }
 }
-
 pub const CSources = struct {
     header_paths: ?[]const []const u8 = null,
     c_sources: ?[]const []const u8 = null,
